e7723cb33aa0534f168301f39cfc0c28fb1762d8
ivanzz.eth <ivanzz.eth@gmail.com>
2026-01-13 15:16:51 +0800

    fix: limit salt generation range to 2^31 (2147483648)
    - Change GenerateRandomSalt max value from 2^32 to 2^31
    - API requires salt to be between 0 and 2147483648
    - Remove unused math import

--- pkg/utils/salt.go
+++ pkg/utils/salt.go
@@ -2,12 +2,12 @@ package utils
 
 import (
 	"crypto/rand"
-	"math"
 	"math/big"
 )
 
 func GenerateRandomSalt() int64 {
-	maxInt := math.Pow(2, 32)
-	nBig, _ := rand.Int(rand.Reader, big.NewInt(int64(maxInt)))
+	// API requires salt to be between 0 and 2147483648 (2^31)
+	maxInt := int64(2147483648) // 2^31
+	nBig, _ := rand.Int(rand.Reader, big.NewInt(maxInt))
 	return nBig.Int64()
 }
